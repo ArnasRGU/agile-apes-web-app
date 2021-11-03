cb21432a9bcb0f5485d45bda83a1736b140cccce
John <j.leith1@rgu.ac.uk>
2021-11-03 14:54:56 +0000

    server.js changes

--- server.js
+++ server.js
@@ -14,5 +14,37 @@ app.get("/", function(req, res) {
     res.sendFile("public/html/create_account.html", { root: __dirname });
 });
 
+app.get("/createsession", function(req, res) {
+    res.sendFile("public/html/create_session.html", { root: __dirname });
+});
+
+app.get("/editaccount", function(req, res) {
+    res.sendFile("public/html/edit_account.html", { root: __dirname });
+});
+
+app.get("/editsession", function(req, res) {
+    res.sendFile("public/html/edit_session.html", { root: __dirname });
+});
+
+app.get("/login", function(req, res) {
+    res.sendFile("public/html/login.html", { root: __dirname });
+});
+
+app.get("/navpage", function(req, res) {
+    res.sendFile("public/html/navigation_page.html", { root: __dirname });
+});
+
+app.get("/sessionadmin", function(req, res) {
+    res.sendFile("public/html/session_admin.html", { root: __dirname });
+});
+
+app.get("/sessionpages", function(req, res) {
+    res.sendFile("public/html/session_pages.html", { root: __dirname });
+});
+
+app.get("/viewprofiles", function(req, res) {
+    res.sendFile("public/html/view_profiles.html", { root: __dirname });
+});
+
 app.listen(8080);
 console.log('Server started at http://localhost:' + 8080);
